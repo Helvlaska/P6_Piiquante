e4158bd853259d1b87fef537d2db8b911f4b5b1d
Claire Castan <ccastan@live.fr>
2023-08-31 10:57:56 +0200

    :hammer: (app.js) Création d'un fichier app

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
     res.end('Voilà la réponse du serveur !');
 });
 /*nom de la fonction = const précé.methode(fonction anonyme(arguments,arguments)=>{
-    ce que la fonction doit faire
+    ce que la fonction doit
 }) */
 server.listen(process.env.PORT || 3000);
