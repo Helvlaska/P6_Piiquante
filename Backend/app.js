845b6bd3e788b060c66e6d5c2c1cdda6d9dc50a0
Claire Castan <ccastan@live.fr>
2023-08-31 11:18:00 +0200

    :sparkles: (app.js) Ajout de CORS

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,16 @@ const express = require('express');
 //express run
 const app = express();
 
-//middleware
+/**********MIDDLEWARE**********/
+
+//Cors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
