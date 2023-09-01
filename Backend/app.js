267c6752cbf336d2759ce987f6b168aac99764d1
Claire Castan <ccastan@live.fr>
2023-09-01 11:30:39 +0200

    :recycle: (app.js) Ajout de Mongoose et connection avec la base de données MongoDB

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,7 @@
-//ce dont j'ai besoin ...
+/**********CE DONT J'AI BESOIN**********/
 const express = require('express');
+//pas besoin d'installer Bodyparser déjà intégré dans Express
+const mongoose = require('mongoose');
 
 //express run
 const app = express();
@@ -13,6 +15,17 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+//Express récupère les données du body en format JSON
+app.use(express.json());
+
+//Connection a la base de données MongoDB
+mongoose.connect('mongodb+srv://Helvlaska:lnORD605303@atlascluster.w3qjnpw.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
