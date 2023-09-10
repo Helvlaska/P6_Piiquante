e7fc1a532020683fdd9e274b2fc368f7ecf4713b
Claire Castan <ccastan@live.fr>
2023-09-10 11:06:05 +0200

    :sparkles: (routes/user.js) Ajout des routes ID/MDP

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,9 +1,11 @@
 /**********CE DONT J'AI BESOIN**********/
 const express = require('express');
 const router = express.Router();
+const userCtrl = require('../controllers/user');
 
-/**********MODELS UTILISES**********/
-
+/**********CONTROLLERS UTILISES**********/
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
 //routes/user importable partout dans le projet
 module.exports = router;
