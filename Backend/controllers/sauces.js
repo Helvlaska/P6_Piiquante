173609582af6485cb98a433540b76b1536fd8c37
Claire Castan <ccastan@live.fr>
2023-09-10 18:56:56 +0200

    :sparkles: (controllers/sauces.js) Ajout d'un fichier sauces et de la logique métier pour ajouter/modifier/chercher/supprimer/récupérer les/la sauce(s)

---
+++ Backend/controllers/sauces.js
@@ -0,0 +1,101 @@
+/**********CE DONT J'AI BESOIN**********/
+const Sauce = require('../models/sauces');
+
+/**********LOGIQUE METIER**********/
+//Ajout d'une nouvelle sauce
+exports.createSauce = (req, res, next) => {
+  const sauce = new Sauce({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  sauce.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+//Récupérer une sauce
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({
+    _id: req.params.id
+  }).then(
+    (sauce) => {
+      res.status(200).json(sauce);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+
+//Modifier une sauce
+exports.modifySauce = (req, res, next) => {
+  const sauce = new Sauce({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  Sauce.updateOne({_id: req.params.id}, sauce).then(
+    () => {
+      res.status(201).json({
+        message: 'Sauce updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+//Supprimer une sauce
+exports.deleteSauce = (req, res, next) => {
+    Sauce.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+//Récuperer toutes les sauces
+exports.getAllSauce = (req, res, next) => {
+    Sauce.find().then(
+    (sauce) => {
+      res.status(200).json(sauce);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
