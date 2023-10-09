2a98fda47de6e8bb1459e81ba4fad5ce9f5ac928
Claire Castan <ccastan@live.fr>
2023-10-09 10:16:46 +0200

    :speech_balloon: (routes/user.js) Ajout de commentaires

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,11 +1,11 @@
 /**********CE DONT J'AI BESOIN**********/
 const express = require('express');
 const router = express.Router();
-const userCtrl = require('../controllers/user');
+const userCtrl = require('../controllers/user'); // import du modèle user
 
 /**********ROUTES**********/
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', userCtrl.signup); // envoie un nouveau user à la base de données 
+router.post('/login', userCtrl.login); // envoie et vérifie les données de connection du user via la base de données 
 
 // importable partout dans le projet
 module.exports = router;
